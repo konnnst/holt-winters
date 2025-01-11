829de224499e8b45ea0756c5e843ce69cc0fdde4
konstantin.ovsyannikov <st107930@gmail.com>
2025-01-11 23:49:06 +0300

    wip: print numbers ffrom bd

--- pg_hw.c
+++ pg_hw.c
@@ -7,6 +7,9 @@
 #include "postgresql/server/utils/builtins.h"
 #include <postgresql/server/catalog/pg_type_d.h>
 #include <postgresql/server/executor/spi.h>
+#include <postgresql/server/executor/executor.h>
+#include "postgresql/server/funcapi.h"
+#include <postgresql/server/access/htup.h>
 
 #include "hw_api.h"
 
@@ -14,19 +17,10 @@
 PG_MODULE_MAGIC;
 #endif
 
+#define MAX_SIZE 100000
 
-//PG_FUNCTION_INFO_V1(add_two);
-//
-//Datum
-//add_two(PG_FUNCTION_ARGS)
-//{
-//    int32 arg = PG_GETARG_INT32(0);
-//
-//    PG_RETURN_INT32(arg + 2);
-//}
 
 PG_FUNCTION_INFO_V1(hw_forecast_manual);
-
 
 Datum
 hw_forecast_manual(PG_FUNCTION_ARGS)
@@ -40,23 +34,32 @@ hw_forecast_manual(PG_FUNCTION_ARGS)
 //    float8 gamma = PG_GETARG_FLOAT8(5);
 //    double array[3] = {1, 2, 3};
 
-    SPI_execute("SELECT series FROM series_table", true, 0);
-    SPI_tuptable->numvals;
+    int status = 0, series_length = 0;
+    if (SPI_connect() == SPI_OK_CONNECT && SPI_execute("SELECT series FROM series_table", true, 0) == SPI_OK_SELECT && SPI_tuptable != NULL) {
+        series_length = (int)SPI_tuptable->numvals;
+    }
 
     ArrayType *result;
-    Datum numbers[3];
+    Datum series_prepared[MAX_SIZE];
     int16 typlen;
     bool typbyval;
     char typalign;
-    numbers[0] = 1;
-    numbers[1] = 2;
-    numbers[2] = 3;
+ 
+    TupleDesc tupdesc = SPI_tuptable->tupdesc;
+    SPITupleTable *tuptable = SPI_tuptable;
+    series_length = SPI_processed;
+    for (int i = 0; i < series_length; i++) {
+        HeapTuple tuple = tuptable->vals[i];
+        series_prepared[i] = atof(SPI_getvalue(tuple, tupdesc, 1));
+    }
 
+    get_typlenbyvalalign(FLOAT8OID, &typlen, &typbyval, &typalign);
+    result = construct_array(series_prepared, series_length, FLOAT8OID, typlen, typbyval, typalign);
 
-    get_typlenbyvalalign(INT4OID, &typlen, &typbyval, &typalign);
-    result = construct_array(numbers, 3, INT4OID, typlen, typbyval, typalign);
+    SPI_finish();
 
     PG_RETURN_ARRAYTYPE_P(result);
+
 }
 //
 //
@@ -71,4 +74,3 @@ hw_forecast_manual(PG_FUNCTION_ARGS)
 //
 //    PG_RETURN_INT32(series_length + season_length + forecast_length);
 //}
-//
